7c2017e0ee61a23ec35dca00fb3b12ac4a41cbb2
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2023-01-15 23:18:08 -0300

    first commit

---
+++ src/main/kotlin/com/rodao/restwithspringbootandkotlinerudio/RestWithSpringBootAndKotlinErudioApplication.kt
@@ -0,0 +1,11 @@
+package com.rodao.restwithspringbootandkotlinerudio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinErudioApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinErudioApplication>(*args)
+}
---
+++ src/test/kotlin/com/rodao/restwithspringbootandkotlinerudio/RestWithSpringBootAndKotlinErudioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rodao.restwithspringbootandkotlinerudio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestWithSpringBootAndKotlinErudioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
